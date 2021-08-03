97a88d466efdfe4b426a28b529362a56ae60a90f
Joyce Lee <joycelee901@gmail.com>
2021-08-03 13:05:38 -0500

    commit tree a9d683835f25386e37fb5aaac42ada173a836a29

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: {error: user.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user
+            # {id: user.id, username: user.username}
+        else
+            render json: { error: "Invalid username or password" }, status: :unauthorized
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
+end
